cc34e6f588db512d94dd99a68e7f0409108150c2
Una Chang <una.c303@outlook.com>
2021-08-11 22:21:38 +0800

    include、html頁面拆解

---
+++ b.php
@@ -0,0 +1,12 @@
+<?php include __DIR__. '/partials/html-head.php'; ?>
+<?php include __DIR__. '/partials/navbar.php'; ?>
+
+<div class="container">
+    <h2>hello world</h2>
+</div>
+
+<?php include __DIR__. '/partials/scripts.php'; ?>
+<?php include __DIR__. '/partials/html-foot.php'; ?>
+
+<!-- 網頁切分方便修改＆維護，假設網頁中navbar相同，就只要改一份檔案
+ -->
