1ba32d1a602eec56adb5502f3ae41ec5cdaf9b44
Una Chang <una.c303@outlook.com>
2021-08-17 00:32:35 +0800

    隨機假資料

--- fake-data.php
+++ fake-data.php
@@ -17,15 +17,25 @@ $sql = "INSERT INTO `address_book_0814`(
 $stmt = $pdo->prepare($sql);
 
 
+//姓名亂數
+$str = '一位日本法學教師去星巴克點飲料意外發現店員也在杯身上寫下六法全書的內容圖擷取自推特';
+$chrArray = preg_split('//u', $str, -1, PREG_SPLIT_NO_EMPTY);
+
+
 //for內部為要執行的內容
+//產生100筆假資料
 for($i=0; $i<100; $i++){
+
+    shuffle($chrArray);
+    // $ar = array_slice($chrArray, 0, 3);
+    $name = implode('', array_slice($chrArray, 0, 3)); //跟$ar定義列合併
     
     $stmt->execute([
-        $_POST['name'], 
-        $_POST['email'], 
-        $_POST['mobile'], 
-        $_POST['birthday'], 
-        $_POST['address'], 
+        $name, 
+        uniqid(). '@test.com', 
+        '09'.strval(rand(10000000, 99999999)),  //strval轉換為字串；rand(範圍區間)
+        date('Ｙ-m-d', rand(strtotime('1990-01-01'), strtotime('2000-12-31'))), //在此區間內的隨機日期
+        '台北市', 
     ]);
 
 
